f7162388af65a4bd117961a8653c73efec17cd96
BhavaniNaroju <112704417+BhavaniNaroju@users.noreply.github.com>
2022-10-04 13:54:05 +0530

    Create Registration_db.php

---
+++ Registration_db.php
@@ -0,0 +1,22 @@
+<?php
+ include("db-connect.php)
+ if(isset($_POST[signup"])
+  {
+  $username=$_POST["Username"];
+  $Password=$_post["password"];
+  $email=$_post["email"];
+  $query="insert into login(username,password,email);
+       values($username,$password,$email)";
+  if(musqli_query($conn,$query))
+  {
+   echo "Registration Successfull";
+  }else{
+   echo "Registration unsuccessful";
+  }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
